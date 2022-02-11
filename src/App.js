67da4c3aaa9e7c34724cc51023219adabbf4ff82
Shams Arefin Khan <robeeen@gmail.com>
2022-02-11 22:36:49 +0600

    Update

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import './App.css';
+
+const App = () => {
+  const[result, setResult] = useState("hi")
+  return(
+    <>
+    <div className="container">
+      <form action="">
+        <input type="text" value={result}/>
+      </form>
+     <div className="keypad">
+       <button>Clear</button>
+       <button>C</button>
+       <button>&divide;</button>
+       <button>7</button>
+       <button>8</button>
+       <button>9</button>
+       <button>&times;</button>
+       <button>4</button>
+       <button>5</button>
+       <button>6</button>
+       <button>&mdash;</button>
+       <button>1</button>
+       <button>2</button>
+       <button>3</button>
+       <button>+</button>
+       <button>0</button>
+       <button>.</button>
+       <button>=</button>
+     </div>
+    </div>
+    </>
+  )
+}
+export default App;
