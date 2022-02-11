556948f55e063bd103dcad6b8d8c14fca4b2238c
Shams Arefin Khan <robeeen@gmail.com>
2022-02-11 23:23:29 +0600

    Final

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ const App = () => {
     setResult("");
   }
   const backspace = () => {
-
+    setResult(result.slice(0,  -1));
+  }
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error")
+    }
   }
   return(
     <>
@@ -20,24 +27,24 @@ const App = () => {
         <input type="text" value={result}/>
       </form>
      <div className="keypad">
-       <button onClick={clear} id="clear">Clear</button>
-       <button onClick={backspace} id="backspace">C</button>
-       <button value="/" onClick={handleClick}>&divide;</button>
+       <button className="amber" onClick={clear} id="clear">Clear</button>
+       <button className="amber" onClick={backspace} id="backspace">C</button>
+       <button className="amber" value="/" onClick={handleClick}>&divide;</button>
        <button value="7" onClick={handleClick}>7</button>
        <button value="8" onClick={handleClick}>8</button>
        <button value="9" onClick={handleClick}>9</button>
-       <button value="*" onClick={handleClick}>&times;</button>
+       <button className="amber" value="*" onClick={handleClick}>&times;</button>
        <button value="4" onClick={handleClick}>4</button>
        <button value="5" onClick={handleClick}>5</button>
        <button value="6" onClick={handleClick}>6</button>
-       <button value="-" onClick={handleClick}>&mdash;</button>
+       <button className="amber" value="-" onClick={handleClick}>&mdash;</button>
        <button value="1" onClick={handleClick}>1</button>
        <button value="2" onClick={handleClick}>2</button>
        <button value="3" onClick={handleClick}>3</button>
-       <button value="+" onClick={handleClick}>+</button>
+       <button className="amber" value="+" onClick={handleClick}>+</button>
        <button value="0" onClick={handleClick}>0</button>
        <button value="." onClick={handleClick}>.</button>
-       <button onClick={handleClick}>=</button>
+       <button className="amber" id="result" onClick={calculate}>=</button>
      </div>
     </div>
     </>
